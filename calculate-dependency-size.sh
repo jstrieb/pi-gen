d4f6e36446a16b0cb2927e64b7d8ea2b6a3c0660
Jacob Strieb <jstrieb@users.noreply.github.com>
2022-07-31 13:46:48 -0400

    Remove unused dependency calculation code

--- calculate-dependency-size.sh
+++ calculate-dependency-size.sh
@@ -19,25 +19,3 @@ cat dependency_graph.txt \
   | sort -n \
   | uniq -f 1 \
   | tee sizes.txt
-
-
-exit
-
-echo stage2/01-sys-tweaks/00-packages \
-  | xargs cat \
-  | tr ' ' '\n' \
-  | xargs apt-cache depends \
-  | tee dependencies.txt
-
-cat dependencies.txt \
-  | grep -v -E 'Breaks|Conflicts|Recommends|Replaces|Suggests' \
-  | sed -E -e 's/ *(\w+:)? *\|?<?([a-zA-Z0-9]+)>?/\2/g' \
-  | xargs apt-cache show \
-  | grep -E "^(Package|Size)" \
-  | cut -d " " -f 2 \
-  | xargs -L 2 echo \
-  | sed -E -e 's/(.*) (.*)/\2 \1/g' \
-  | sort -n \
-  | uniq -f 1 \
-  | tee sizes.txt
-
