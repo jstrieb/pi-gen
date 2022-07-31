b7970809169255625cc840f939f38c9917f1d218
Jacob Strieb <jstrieb@users.noreply.github.com>
2022-07-31 16:00:00 -0400

    Fix regex bugs and remove big deps

--- calculate-dependency-size.sh
+++ calculate-dependency-size.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 if [ ! -f dependency_graph.txt ]; then
-  # echo stage2/01-sys-tweaks/00-packages \
-  find . -type f -iname "*packages*" \
+  echo Calculating dependencies...
+
+  # find . -type f -iname "*packages*" \
+  echo stage2/01-sys-tweaks/00-packages \
     | xargs cat \
     | tr ' ' '\n' \
     | xargs -L 1 debtree \
     | tee dependency_graph.txt
 fi
 
+echo Calculating sizes...
+
 cat dependency_graph.txt \
-  | grep -o -E '"\w+" -> "\w+"' \
-  | sed -E -e 's/"(\w+)" -> "(\w+)"/\1\n\2/g' \
+  | grep -o -E '".+" -> ".+"' \
+  | sed -E -e 's/"(.+)" -> "(.+)"/\1\n\2/g' \
   | sort \
   | uniq \
   | xargs apt-cache show \
-  | grep -E "^(Package|Installed-Size)" \
+  | grep -E "^(Package|Size)" \
   | cut -d " " -f 2 \
   | xargs -L 2 echo \
-  | sed -E -e 's/(.*) (.*)/\2 \1/g' \
-  | sort -n \
+  | sort -k 2 -n \
   | uniq -f 1 \
   | tee sizes.txt
